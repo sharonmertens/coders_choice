121f05f5d7615f185670fd7cd9a96b15707bbf64
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-02-06 20:18:25 -0500

    created CREATE route and mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Places = require('../models/places.js');
+
+// CREATE route
+router.post('/', (req, res) => {
+  Places.create(req.body, (err, createdPlace) => {
+    res.json(createdPlace);
+  });
+});
+
+
 router.get('/', (req, res) => {
   res.send('index');
 });
